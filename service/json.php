1f47f719b17448071c4331880dd85adcbecbac88
Alexzander thunder Shevchenko <thunder@blackdeath.ru>
2013-02-06 17:09:59 +0400

    added ORDER BY `date` in MySQL query.

--- service/json.php
+++ service/json.php
@@ -1,4 +1,4 @@
-﻿<?
+<?
 require_once('../conf/const.php');
 require_once('../conf/db.php');
 require_once('../conf/functions.php');
@@ -7,6 +7,6 @@ $def_lang = 'ru';
 $cur_lang = filter_input(INPUT_GET, "language", FILTER_SANITIZE_STRING);
 if ($cur_lang == NULL || $cur_lang === false || !in_array($cur_lang, $_avail_langs) ) { $cur_lang = $def_lang; }
 
-echo base64_encode(serialize( makeQueryArray("SELECT id, link, title, body as `description`, `date` as `time`, `comments_link` as `comment` FROM `{$tables['wot-news']}` WHERE `lang`='{$cur_lang}' AND `server`='".get_site_domain()."'") ));
+echo base64_encode(serialize( makeQueryArray("SELECT id, link, title, body as `description`, `date` as `time`, `comments_link` as `comment` FROM `{$tables['wot-news']}` WHERE `lang`='{$cur_lang}' AND `server`='".get_site_domain()."' ORDER BY `date` DESC") ));
 
 ?>
